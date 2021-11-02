dafe24f9e5d2668b704ad8673616a66e4ed80f45
Halil Dogan <33289@ma-web.nl>
2021-11-02 15:53:45 +0100

    Text based aplicatie

--- Textbased_apllication.py
+++ Textbased_apllication.py
@@ -1,3 +1,102 @@
+def vraag13 ():
+
+    print("Hebben jullie moeite gehad bij het vluchten?")
+    print("a = Ja.")
+    print("b = Nee.")
+
+    antwoord13 = input ()
+    if antwoord13.lower() == "a":
+        print("Ja heel erg veel was wel te doen.")
+        print(vraag14())
+    elif antwoord13.lower() == "b":
+        print("Nee was heel erg makkelijk geen problemen gehad.")
+        print(vraag13())
+    else: 
+        print("Kies a,b")
+
+    print(vraag13())
+
+
+
+def vraag12 ():
+
+    print("Onderweg kregen wij eten we hadden hoger.")
+    print("a = Aangenomen.")
+    print("b = Niet aangenomen.")
+
+    antwoord12 = input ()
+    if antwoord12.lower() == "a":
+        print("Dan kan je nog een tijdje door.")
+        print(vraag13())
+    elif antwoord12.lower() == "b":
+        print("Dan ga je dood door honger.")
+        print(vraag11())
+    else: 
+        print("Kies a,b")
+
+    print(vraag12())
+
+
+
+def vraag11 ():
+
+    print("Waar naar toe is hij gegaan?")
+    print("a = Hij was al naar Nederland gegaan.")
+    print("b = Hij komt nog.")
+
+    antwoord11 = input ()
+    if antwoord11.lower() == "a":
+        print("Is hij alles van te voren gaan regelen?")
+        print(vraag10())
+    elif antwoord11.lower() == "b":
+        print("Waarom zijn jullie niet samen gevlucht?")
+        print(vraag12())
+    else: 
+        print("Kies a,b")
+
+    print(vraag11())
+
+
+def vraag10 ():
+
+    print("Is hij daar gebleven?")
+    print("a = Nee")
+    print("b = Ja")
+
+    antwoord10 = input ()
+    if antwoord10.lower() == "a":
+        print("Dat is veiliger dat hij mee is.")
+        print(vraag11())
+    elif antwoord10.lower() == "b":
+        print("Dat is niet veilig maar is hij nog levend.")
+        print(vraag11())
+   
+    else: 
+        print("Kies a,b")
+
+    print(vraag10())
+    
+    
+def vraag9 ():
+
+    print("Heb je een vader?")
+    print("a = Ja")
+    print("b = Nee")
+ 
+
+    antwoord9 = input ()
+    if antwoord9.lower() == "a":
+        print("Waar is hij?")
+        print(vraag10())
+    elif antwoord9.lower() == "b":
+        print("Dat is niet leuk geconduleerd ")
+        print(vraag10())
+    else: 
+        print("Kies a,b")
+
+    print(vraag9())
+    
+    
 def vraag8 ():
 
     print("Met wat vlucht je")
@@ -14,11 +113,11 @@ def vraag8 ():
         print(vraag9())
     elif antwoord8.lower() == "c":
         print("Dan duurt het heel lang en kan je dood gaan")
-        print(vraag9())
+        print(vraag8())
     else: 
         print("Kies a,b of c")
 
-    print(vraag6())
+    print(vraag8())
 
 
 def vraag7 ():
@@ -30,8 +129,8 @@ def vraag7 ():
 
     antwoord7 = input ()
     if antwoord7.lower() == "a":
-        print("Dat is Munchen")
-        print(vraag8())
+        print("Dat is Munchen denk even nog een keer na.")
+        print(vraag6())
     elif antwoord7.lower() == "b":
         print("Dat is hollandse glorie")
         print(vraag8())
@@ -55,7 +154,7 @@ def vraag6 ():
         print("Dat is ook verstandig, denk nog een keer.")
         print(vraag5())
     else: 
-        print("Kies a,b of c")
+        print("Kies a,b")
 
     print(vraag6())
 
@@ -144,7 +243,7 @@ def vraag2 ():
         print("Vluchten is het verstandigste")
         print(vraag3())
     else: 
-        print("Kies a,b of c")
+        print("Kies a,b")
 
     print(vraag2())
 
